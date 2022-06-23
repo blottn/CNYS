f37955d3ce980785b870ecd235dd8b6d34fcb51d
Nicholas Blott <blottn@tcd.ie>
2022-06-23 15:06:58 +0100

    Added heartbeat

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let room = window.location.pathname;
 
-let local_uid = new Date().getTime();
+let local_uid = new Date().getTime(); // TODO use mac address or something + chrome tab
+let peers = {};
+
+// 1.5s
+const heartbeatWindow = 3000;
+const heartbeatAllowedMisses = 3;
 
 console.log('connecting to wrmhole at:');
 console.log(`ws://wrm.blottn.ie${room}`);
@@ -29,17 +34,65 @@ ws.addEventListener('message', async function (event) {
 
     let { uid } = data;
     createBox(uid);
+    peers[uid] = {
+      last: new Date().getTime(),
+      missed: 0,
+    };
   }
   else if (kind === 'msg') {
     let {delay, uid, content} = data;
+    if (!(uid in peers)) {
+      createBox(uid);
+      peers[uid] = {
+        last: new Date().getTime(),
+        missed: 0,
+      };
+    }
     setTimeout(() => {
       document.getElementById(`uid-${uid}`).value = content;
     }, delay);
   } else if (kind === 'joinack') {
     let { uid } = data;
     createBox(uid);
+    peers[uid] = {
+      last: new Date().getTime(),
+      missed: 0,
+    };
+  } else if (kind === 'heartbeat') {
+    let { uid } = data;
+    console.log(`Received heartbet from ${uid}`);
+    peers[uid] = {
+     last: new Date().getTime(),
+     missed: 0,
+    }
   }
 });
+
+const heartbeatInterval = 1000;
+let heartbeat = () => {
+  ws.send(JSON.stringify({
+    kind: 'heartbeat',
+    data: {
+      uid: local_uid,
+    }
+  }));
+  let now = new Date().getTime();
+  Object.keys(peers).map(peer => {
+    if (now - peers[peer].last > heartbeatWindow) {
+      console.log(`${peer} missed heartbeat`);
+      peers[peer].missed += 1;
+    }
+    if (peers[peer].missed > heartbeatAllowedMisses) {
+      delete peers[peer];
+      // yuck lol
+      // TODO change to id-ing the root box object;
+      let box = document.getElementById(`uid-${peer}`).parentElement.parentElement;
+      box.parentElement.removeChild(box);
+    }
+  });
+  setTimeout(heartbeat, heartbeatInterval);
+}
+setTimeout(heartbeat, heartbeatInterval);
 
 window.addEventListener('load', () => {
   let input = document.getElementById('inputspace');
@@ -54,6 +107,7 @@ window.addEventListener('load', () => {
     }));
   });
 });
+
 
 function createBox(uid) {
   let template = document.getElementById('template');
